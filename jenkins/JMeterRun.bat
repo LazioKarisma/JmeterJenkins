2d777f3722e168c676be2fd10e5a61d744893f8e
LAZIO KHARISMA <Lazio_Kharisma@manulife.com>
2022-08-17 10:08:53 +0700

    modify

--- jenkins/JMeterRun.bat
+++ jenkins/JMeterRun.bat
@@ -1,5 +1,5 @@
 @echo off
 
-set jmeter=C:\developer\JMeter\apache-jmeter-5.5\bin\
+set jmeter=C:\developer\JMeter\apache-jmeter-5.5\bin\jmeter.bat
 
-"%jmeter%" -e -n -t ${WORKSPACE}/jenkins.io.jmx -o html -l ${WORKSPACE}/jenkins/result.jtl
+"%jmeter%" -e -n -t ${WORKSPACE}\jenkins.io.jmx -o html -l ${WORKSPACE}\jenkins\result.jtl
