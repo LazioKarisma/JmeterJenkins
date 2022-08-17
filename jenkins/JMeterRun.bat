6970ef7fe900fccf2509e6d974559b3aa80edc38
LAZIO KHARISMA <Lazio_Kharisma@manulife.com>
2022-08-17 10:12:03 +0700

    test

--- jenkins/JMeterRun.bat
+++ jenkins/JMeterRun.bat
@@ -2,4 +2,4 @@
 
 set jmeter=C:\developer\JMeter\apache-jmeter-5.5\bin\jmeter.bat
 
-"%jmeter%" -e -n -t ${WORKSPACE}\jenkins\jenkins.io.jmx -o html -l ${WORKSPACE}\jenkins\result.jtl
+"%jmeter%" -e -n -t .\jenkins\jenkins.io.jmx -o html -l .\jenkins\result.jtl
